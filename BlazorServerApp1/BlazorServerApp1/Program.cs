ad8a1ba438e405aae3eadfb28dccca2fa140078a
rwecho <rwecho@live.com>
2023-11-28 18:14:15 +0800

    initialize

---
+++ BlazorServerApp1/BlazorServerApp1/Program.cs
@@ -0,0 +1,40 @@
+using BlazorServerApp1.Client.Pages;
+using BlazorServerApp1.Components;
+using BlazorServerApp1.Components.Pages;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<Home>();
+builder.Services.AddSingleton<Counter>();
+builder.Services.AddSingleton<Weather>();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+app.Run();
